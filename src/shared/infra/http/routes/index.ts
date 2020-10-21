d5454883f14184339ec1a2eea9a57b62053bc81f
Leonardo Sampaio Cairo <leocairos@gmail.com>
2020-10-21 14:23:06 -0300

    GET to check service

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,10 @@ import dialogflowFulfillmentRouter from '@modules/dialogflowFulfillment/dialogfl
 
 const routes = Router();
 
+routes.get('/', (request, response) => {
+  return response.sendStatus(200);
+});
+
 routes.use('/dialogflowFulfillment', dialogflowFulfillmentRouter);
 
 export default routes;
